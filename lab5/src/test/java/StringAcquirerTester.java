2adb112a1af01291f5f369fb8286c3543c8a0abe
xlollipopx <anton.stelmax@bk.ru>
2020-10-07 07:17:18 +0300

    lab5

--- lab5/src/test/java/StringAcquirerTester.java
+++ lab5/src/test/java/StringAcquirerTester.java
@@ -1,20 +1,16 @@
 import com.epam.task.five.data.FileStringAcquirer;
 import com.epam.task.five.exception.DataException;
-import com.epam.task.five.logic.StringProcessorByCharSequence;
-import com.epam.task.five.logic.StringProcessorByStringMethods;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.io.File;
-
 public class StringAcquirerTester {
 
-    private static final String TEST_STRING = "src/test/java/resources/input1.txt";
+    private static final String TEST_FILE = "src/test/java/resources/input1.txt";
 
     @Test
     public void testFileStringAcquirer() throws DataException {
         //given
-        FileStringAcquirer fileStringAcquirer = new FileStringAcquirer(TEST_STRING);
+        FileStringAcquirer fileStringAcquirer = new FileStringAcquirer(TEST_FILE);
         String actualString = fileStringAcquirer.readString();
         //when
         String expectedString = "pa ap rqepa";
--- lab5/src/test/java/StringProcessorTester.java
+++ lab5/src/test/java/StringProcessorTester.java
@@ -1,4 +1,5 @@
 import com.epam.task.five.logic.StringProcessorByCharSequence;
+import com.epam.task.five.logic.StringProcessorByRegularExpressions;
 import com.epam.task.five.logic.StringProcessorByStringMethods;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,6 +23,17 @@ public class StringProcessorTester {
         //given
         StringProcessorByCharSequence stringProcessorByCharSequence = new StringProcessorByCharSequence();
         String actualString = stringProcessorByCharSequence.fixString(TEST_STRING);
+        //when
+        String expectedString = "po fvpojfnv po";
+        //then
+        Assert.assertEquals(expectedString, actualString);
+    }
+
+    @Test
+    public void testStringProcessorByRegularExpression() {
+        //given
+        StringProcessorByRegularExpressions stringProcessorByRegularExpressions = new StringProcessorByRegularExpressions();
+        String actualString = stringProcessorByRegularExpressions.fixString(TEST_STRING);
         //when
         String expectedString = "po fvpojfnv po";
         //then
